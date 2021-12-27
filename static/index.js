90262a6f38ff6c180a714b4064a29c1b51c71a6b
tuna cetin <mrtunacetin@gmail.com>
2021-12-27 20:08:58 +0100

    27Dec21 Tr

--- static/index.js
+++ static/index.js
@@ -2,8 +2,6 @@ const baseUrl = 'https://tunacetin.github.io/web-tutorial-html-css/';
 
 const iframe = document.getElementById('content');
 
-
 function setContent(content) {
   iframe.setAttribute('src', baseUrl + content);
-  // console.log(baseUrl + content);
 }
