90aa21d900b34b60f1f1f5f5e80e62fc157a818d
tuna cetin <mrtunacetin@gmail.com>
2021-12-29 00:50:31 +0100

    courses menu and animation added

--- static/index.js
+++ static/index.js
@@ -1,7 +1,35 @@
 const baseUrl = 'https://tunacetin.github.io/web-tutorial-html-css/';
+let menuVisibility = false;
 
 const iframe = document.getElementById('content');
+const menu = document.getElementById('menu');
+const menuIcon = document.getElementById('menuIcon');
+const closeIcon = document.getElementById('closeIcon');
 
 function setContent(content) {
   iframe.setAttribute('src', baseUrl + content);
+}
+
+function handleIconToggle() {
+  if (menuVisibility) {
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "inline";
+  } else {
+    menuIcon.style.display = "none";
+    closeIcon.style.display = "inline";
+  }
+}
+
+function handleMenuToggle() {
+  if (menuVisibility) {
+    menu.classList.remove('closed');
+  } else {
+    menu.classList.add('closed');
+  }
+}
+
+function toggleMenu() {
+  handleIconToggle();
+  handleMenuToggle();
+  menuVisibility = !menuVisibility;
 }
