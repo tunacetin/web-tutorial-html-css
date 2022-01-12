531c12c80ff411c5ec4d71825a71f1a84b7d1382
tuna cetin <mrtunacetin@gmail.com>
2022-01-12 21:07:53 +0100

    12 January 2022

---
+++ 12Jan22/tr/js/index.js
@@ -0,0 +1,19 @@
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
+
+function add() {
+  document.getElementById('result').innerHTML = 2 + 2;
+}
+
+function minus() {
+  document.getElementById('result').innerHTML = 2 - 2;
+}
+
+function multiply() {
+  document.getElementById('result').innerHTML = 2 * 2;
+}
+
+function divide() {
+  document.getElementById('result').innerHTML = 2 / 2;
+}
