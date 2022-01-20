c6dcd7612dca15790271b6da7f32dae8c478dd50
tuna cetin <mrtunacetin@gmail.com>
2022-01-20 13:46:43 +0100

    17 january 2022

---
+++ 17Jan22/tr/js/index.js
@@ -0,0 +1,58 @@
+let x = 5;
+let y = 6;
+let z = x + y;
+console.log(x);
+console.log(y);
+console.log(z);
+
+const variable = "abcd";
+const variablE = "abcE";
+console.log(variable);
+console.log(variablE);
+
+let a = 5;
+console.log(a);
+a = a + 5;
+console.log(a);
+
+let carName;
+console.log(carName);
+carName = "volvo"
+console.log(carName);
+
+var carName1 = "Volvo";
+var carName1;
+console.log(carName1);
+// hata vermez ve değer silinmez
+
+let b = "John" + " " + "Doe";
+console.log(b);
+b = "John" + " " + "Doe" + 6;
+console.log(b);
+let c = "5" + 2 + 3;
+console.log(c);
+c = 2 + 3 + "5";
+console.log(c);
+
+let d = 3 + 2;
+console.log("d: ", d);
+let e = 3 - 2;
+console.log("e :", e);
+let f = 3 * 2;
+console.log("f :", f);
+let g = 3 / 2;
+console.log("g :", g);
+let h = 3 ** 2;
+console.log("h :", h);
+let i = 20 % 7;
+console.log("i :", i);
+
+let j = 5;
+console.log(j);
+j = j + 1;
+console.log(j);
+
+let k = 5;
+console.log(k);
+k++;
+console.log(k);
